6d7e7fa0bc81942f010b894abfe5e0bd40498b36
bahaicheng <bahaicheng@126.com>
2017-11-23 21:56:44 +0800

    add

--- src/main/scala/sparks/streaming/SparkStreamingScala.scala
+++ src/main/scala/sparks/streaming/SparkStreamingScala.scala
@@ -1,11 +1,15 @@
 package sparks.streaming
 
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 
 /**
  * Created by DELL on 2017/11/23.
  */
 object SparkStreamingScala {
     def main(args: Array[String]) {
-
+        val conf = new SparkConf()
+        val jssc = new StreamingContext(conf,Seconds(2))
     }
 }
