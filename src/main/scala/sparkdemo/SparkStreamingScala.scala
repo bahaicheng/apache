bf079bfcd4ae9952fc62a1ece65ee6fd885de622
unknown <bahaicheng@126.com>
2017-12-18 17:35:06 +0800

    update_pom

--- src/main/scala/sparkdemo/SparkStreamingScala.scala
+++ src/main/scala/sparkdemo/SparkStreamingScala.scala
@@ -2,6 +2,7 @@ package sparkdemo
 
 import kafka.serializer.StringDecoder
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
