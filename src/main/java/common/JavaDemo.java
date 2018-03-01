5cee1d2434339faffd1a1ddf0026bc32ad5a287c
bahaicheng <bahaicheng@126.com>
2018-03-01 10:44:16 +0800

    master

--- src/main/java/common/JavaDemo.java
+++ src/main/java/common/JavaDemo.java
@@ -41,9 +41,5 @@ public class JavaDemo{
         String[] arr = {"aa", "bb", "cc"};
         List<String> strings = Arrays.asList(arr);
 
-
-        Set<String> set = new HashSet<>();
-        set.add("bourne");
-        Iterator<String> iterator = set.iterator();
     }
 }
